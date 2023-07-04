47ea819a4732f0beacbcf057ef3ecafadfd47e11
savelazquez <40502943+savelazquez@users.noreply.github.com>
2023-07-04 16:34:33 -0300

    Practico_02/ej2

--- practico_02/source/ejercicio_02.py
+++ practico_02/source/ejercicio_02.py
@@ -11,16 +11,20 @@ $PATH$/practico_02> python -m source.ejercicio_02
 """
 
 # Completar
-
+import main as main
+import source.util as util
+import source.data.database as database
+import config.test_config as test_config
+import config.db_config.migrations as migrations
 
 
 
 # NO MODIFICAR - INICIO
-#assert main.name == "main"
-#assert util.name == "util"
-#assert database.name == "database"
-#assert test_config.name == "test_config"
-#assert migrations.name == "migrations"
+assert main.name == "main"
+assert util.name == "util"
+assert database.name == "database"
+assert test_config.name == "test_config"
+assert migrations.name == "migrations"
 # NO MODIFICAR - FIN
 
 
