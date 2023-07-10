d6fe371f1c9e387e5afdaa8d61130d1b4a601932
MartinChelin <chelinmartin20@gmail.com>
2023-07-09 21:50:46 -0300

    pop1
    Error de validacion campos vacios

--- TPIKIVY/CONTROLADOR/controlador.py
+++ TPIKIVY/CONTROLADOR/controlador.py
@@ -1,7 +1,8 @@
 import sqlite3
+from DATOS import clases
 
 def conectar_db():
-    conn = sqlite3.connect('base_datos_kivy.db')
+    conn = sqlite3.connect('D:/Soporte/practicos/G2310/TPIKIVY/base_datos_kivy.db')
     return conn
 
 def comit_cerrar_conexion(conn):
@@ -21,8 +22,8 @@ def create_table():
     comit_cerrar_conexion(conn)
 
 def insert(nombre:str, apellido:str, dni:int, mail:str):
+    clases.Empleado(nombre, apellido, dni, mail)
     conn = conectar_db()
     c = conn.cursor()
-
     c.execute('''INSERT INTO Empleado (nombre, apellido, dni, mail) VALUES (?, ?, ?, ?)''', (nombre, apellido, dni, mail))
     comit_cerrar_conexion(conn)
--- TPIKIVY/UI/vista.py
+++ TPIKIVY/UI/vista.py
@@ -3,11 +3,25 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.lang import Builder
+from kivy.uix.popup import Popup
 from CONTROLADOR import controlador
+
+class MyPopup (Popup):
+    pass
 
 
 class UI(ScreenManager):
     pass
+    def open_pop(self):
+        pops = MyPopup()
+        pops.open()
+
+    def error_vacios(self):
+        if (self.ids.nombre.text and self.ids.apellido.text and self.ids.dni.text and self.ids.mail.text) == "":
+            return False
+        else:
+            return True
+    
     def reset_text(self):
         self.ids.nombre.text = ""
         self.ids.apellido.text = ""
@@ -19,7 +33,7 @@ class UI(ScreenManager):
         apellidox = self.ids.apellido.text
         dnix = self.ids.dni.text
         mailx = self.ids.mail.text
-        controlador.insert( nombrex, apellidox, dnix, mailx)
+        #controlador.insert( nombrex, apellidox, dnix, mailx)
 
 class MyApp(App):
     def build(self):
