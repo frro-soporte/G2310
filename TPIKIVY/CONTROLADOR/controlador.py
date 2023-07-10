10a9ce66cf488310fad78ba3b0f13c0600833fe6
MartinChelin <chelinmartin20@gmail.com>
2023-07-10 16:58:06 -0300

    Controlador
    Hice cambios en el controlador y datos

--- TPIKIVY/CONTROLADOR/controlador.py
+++ TPIKIVY/CONTROLADOR/controlador.py
@@ -1,29 +1,20 @@
 import sqlite3
-from DATOS import clases
+from DATOS import clases as cl
 
-def conectar_db():
+"""def conectar_db():
     conn = sqlite3.connect('D:/Soporte/practicos/G2310/TPIKIVY/base_datos_kivy.db')
     return conn
 
 def comit_cerrar_conexion(conn):
     conn.commit()
-    conn.close()
+    conn.close()"""
 
 def create_table():
-    conn = conectar_db()
-    c = conn.cursor()
+    cl.create_table()
 
-    c.execute('''CREATE TABLE Empleado (
-                nombre CHAR(30),
-                apellido CHAR(30),
-                dni INTEGER PRIMARY KEY,
-                mail CHAR(30)
-            )''')
-    comit_cerrar_conexion(conn)
+def validar_dni(self, dni):
+    return cl.validar_dni(self,dni)
 
-def insert(nombre:str, apellido:str, dni:int, mail:str):
-    clases.Empleado(nombre, apellido, dni, mail)
-    conn = conectar_db()
-    c = conn.cursor()
-    c.execute('''INSERT INTO Empleado (nombre, apellido, dni, mail) VALUES (?, ?, ?, ?)''', (nombre, apellido, dni, mail))
-    comit_cerrar_conexion(conn)
+def insert(self, nombre:str, apellido:str, dni:int, mail:str):
+    cl.insert(self, nombre, apellido, dni, mail)
+    
--- TPIKIVY/DATOS/clases.py
+++ TPIKIVY/DATOS/clases.py
@@ -1,3 +1,5 @@
+import sqlite3
+from DATOS import base_datos as bd
 class Empleado:
     def __init__(self, nombre:str, apellido:str, dni:int, mail:str) -> None:
         pass 
@@ -5,4 +7,38 @@ class Empleado:
         self.apellido = apellido
         self.dni = dni
         self.mail = mail
+
+def validar_dni(self, dni):
+    conn = bd.conectar_db()
+    c = conn.cursor()
+    c.execute("SELECT dni FROM Empleado WHERE dni = ?", dni)
+    result = c.fetchone()
+    if result is None:
+        bd.comit_cerrar_conexion(conn)
+        return True  
+    else:
+        bd.comit_cerrar_conexion(conn)
+        return False
+
+def create_table():
+    conn = bd.conectar_db()
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE Empleado (
+                    nombre CHAR(30),
+                    apellido CHAR(30),
+                    dni INTEGER PRIMARY KEY,
+                    mail CHAR(30)
+                )''')
+    bd.comit_cerrar_conexion(conn)
+
+def insert(self, nombre:str, apellido:str, dni:int, mail:str):
+    Empleado(nombre, apellido, dni, mail)
+    conn = bd.conectar_db()
+    c = conn.cursor()
+    c.execute('''INSERT INTO Empleado (nombre, apellido, dni, mail) VALUES (?, ?, ?, ?)''', (nombre, apellido, dni, mail))
+    bd.comit_cerrar_conexion(conn)
+
+
+
         
