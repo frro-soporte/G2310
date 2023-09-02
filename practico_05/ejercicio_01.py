ff30e90ce8afbeed06ad6b6222d7fb74e3c30775
MartinChelin <chelinmartin20@gmail.com>
2023-09-02 18:00:12 -0300

    practico_05/ejer1

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -1,9 +1,10 @@
 """Base de Datos - Creación de Clase en ORM"""
-
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+engine = create_engine("sqlite:///practico_05/socios.db", echo = True)
 Base = declarative_base()
 
 class Socio(Base):
@@ -14,6 +15,18 @@ class Socio(Base):
         - apellido: string (longitud 250)
     """
     __tablename__ = 'socios'
+    id_socio = Column(Integer(), primary_key=True)
+    dni = Column(Integer())
+    nombre = Column(String(250))
+    apellido =Column(String(250))
 
-    # Completar
+    def __str__(self):
+        return self.dni
+
+Session = sessionmaker(engine)
+session = Session()
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    session.close()
 
