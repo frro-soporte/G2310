eb69924f71b591e15f854fb1e0df7bf64f37ddd9
MartinChelin <chelinmartin20@gmail.com>
2023-10-04 20:44:41 -0300

    TPOPENCV_v1.2
    Pantalla iniciar lista

--- TPOPENCV/CONTROLADOR/controlador.py
+++ TPOPENCV/CONTROLADOR/controlador.py
@@ -3,7 +3,11 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
 import sqlite3
+import cv2
 
 #VALIDACIONES
 #ef validar_tabla():
@@ -11,7 +15,22 @@ import sqlite3
 
 class UI(ScreenManager):
     pass
-   
+    def start_capture(self):
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0 / 30.0)
+
+    def stop_capture(self):
+        self.capture.release()
+        Clock.unschedule(self.update)
+
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            self.ids.camera_image.texture = texture
 #PRINCIPAL
 class MyApp(App):
     def build(self):
